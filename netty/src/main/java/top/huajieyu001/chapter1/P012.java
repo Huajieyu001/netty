db43bd46175a1102d5179937991b41f8594dbda0
Huajieyu001 <331173500@qq.com>
2026-02-13 14:05:46 +0800

    测试复制文件

--- netty/src/main/java/top/huajieyu001/chapter1/P012.java
+++ netty/src/main/java/top/huajieyu001/chapter1/P012.java
@@ -1,5 +1,7 @@
 package top.huajieyu001.chapter1;
 
+import lombok.extern.slf4j.Slf4j;
+
 import java.io.*;
 import java.nio.ByteBuffer;
 import java.nio.channels.FileChannel;
@@ -11,10 +13,11 @@ import java.nio.charset.StandardCharsets;
  * @Version 1.0
  * @Description TODO
  */
+@Slf4j
 public class P012 {
 
     public static void main(String[] args) {
-        test2();
+        test3();
     }
 
     /**
@@ -41,6 +44,26 @@ public class P012 {
                 FileChannel to = new FileOutputStream("to.txt").getChannel();
         ){
             from.transferTo(0, from.size(), to);
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    /**
+     * 尝试复制大于2g的文件
+     */
+    public static void test3(){
+        try(
+                FileChannel from = new FileInputStream("G:\\AAAKey\\RussianPassion\\5_6190393645703629788.mp4").getChannel();
+                FileChannel to = new FileOutputStream("G:\\AAAKey\\RussianPassion\\Copied.mp4").getChannel();
+        ){
+            long start = 0;
+            while(start < from.size()){
+                start += from.transferTo(start, from.size(), to);
+                log.debug(start + "\t" + from.size() + "\t" + to.size());
+            }
         } catch (FileNotFoundException e) {
             throw new RuntimeException(e);
         } catch (IOException e) {
